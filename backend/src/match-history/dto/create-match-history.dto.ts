d5bf92b9b587d0eed9fad5c4760341bd3e623414
Joel king yan Hii <jhii@u91z05s01.42kl.edu.my>
2023-06-19 16:30:56 +0800

    remove comments

--- backend/src/match-history/dto/create-match-history.dto.ts
+++ backend/src/match-history/dto/create-match-history.dto.ts
@@ -1,13 +1,3 @@
-// export class CreateMatchHistoryDto {
-// 	match_uid : number;
-// 	winner_uid : number;
-// 	p1_uid : number;
-// 	p2_uid : number;
-// 	p1_score : number;
-// 	p2_score : number;
-// 	creation_date : Date;
-// }
-
 import { IsNotEmpty, IsOptional } from "class-validator";
 
 export class CreateMatchHistoryDto {
