6363e922c25de3358946e938eca89045c6b8099f
Zer <livefatezz@gmail.com>
2023-06-23 15:22:07 +0800

    chore: idk

---
+++ frontend/src/app/socket/SocketProvider.tsx
@@ -0,0 +1,33 @@
+
+import { useState, createContext, useContext, use, useEffect, ReactNode } from "react";
+import { io, Socket } from 'socket.io-client';
+
+type SocketContextType = Socket<any, any> | null;
+
+type SocketProviderProps = {
+    children: ReactNode;
+};
+
+  
+export const SocketContext = createContext<SocketContextType>(null);
+
+export const SocketProvider =  ({ children }: SocketProviderProps) => {
+    const [socket, setSocket] = useState<SocketContextType>(null);
+    
+    useEffect(() => {
+        const socket = io('http://localhost:3000');
+        socket.on('connect', () => {
+            console.log('Connected to socket server', socket.id);
+        });
+        setSocket(socket);
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
