ad4f057c8a84b15228a07aa77e0c03c2dea76c0e
Zer <livefatezz@gmail.com>
2023-08-04 19:10:10 +0800

    feat: effects for killstreak-owning players in game

--- frontend/src/components/game/GameHeader.tsx
+++ frontend/src/components/game/GameHeader.tsx
@@ -1,21 +1,44 @@
 import useGameStore from "@/store/useGameStore";
 
-const PlayerOneScore = () => {
+const PlayerOne = () => {
   const gameData = useGameStore((state) => state.gameData);
 
   return (
-    <div>
+    <div className="flex space-x-12 items-center">
+      {gameData.playerOne ? (
+        <img
+          width={100}
+          height={100}
+          className="w-[60px] h-[60px] rounded-full object-cover"
+          src={gameData.playerOne.avatar}
+          alt="p1 avatar"
+        />
+      ) : (
+        <div className="w-[60px] h-[60px] rounded-full bg-jetblack" />
+      )}
+
       <p className="text-timberwolf text-3xl font-bold">{gameData.p1Score}</p>
     </div>
   );
 };
 
-const PlayerTwoScore = () => {
+const PlayerTwo = () => {
   const gameData = useGameStore((state) => state.gameData);
 
   return (
-    <div>
+    <div className="flex space-x-12 items-center">
       <p className="text-timberwolf text-3xl font-bold">{gameData.p2Score}</p>
+      {gameData.playerTwo ? (
+        <img
+          width={100}
+          height={100}
+          className="w-[60px] h-[60px] rounded-full object-cover"
+          src={gameData.playerTwo.avatar}
+          alt="p2 avatar"
+        />
+      ) : (
+        <div className="w-[60px] h-[60px] rounded-full bg-jetblack" />
+      )}
     </div>
   );
 };
@@ -23,9 +46,9 @@ const PlayerTwoScore = () => {
 const GameHeader = () => {
   return (
     <div className="flex h-[108px] pt-5 mb-5 items-center justify-center space-x-5">
-      <PlayerOneScore />
+      <PlayerOne />
       <p className="text-timberwolf text-2xl font-bold">:</p>
-      <PlayerTwoScore />
+      <PlayerTwo />
     </div>
   );
 };
---
+++ frontend/src/components/game/KillStreak.tsx
@@ -0,0 +1,61 @@
+import useGameStore from "@/store/useGameStore";
+import * as PIXI from "pixi.js";
+import { useEffect, useRef } from "react";
+
+const KillStreak = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const gameData = useGameStore((state) => state.gameData);
+
+  useEffect(() => {
+    if (gameData.currentStreak !== 0 && ref.current) {
+      const app = new PIXI.Application<HTMLCanvasElement>({
+        autoStart: false,
+        resizeTo: window,
+        backgroundAlpha: 0,
+      });
+      ref.current.appendChild(app.view);
+      PIXI.Assets.load("/game-effects/flame.json").then(() => {
+        const textures = [];
+        let i;
+
+        for (i = 0; i < 32; i++) {
+          const framekey = `tile0${i < 10 ? `0${i}` : i}.png`;
+          const texture = PIXI.Texture.from(framekey);
+          const time = 50;
+
+          textures.push({ texture, time });
+        }
+
+        const anime = new PIXI.AnimatedSprite(textures);
+
+        if (gameData.currentStreak === -1) {
+          app.stage.removeChild(anime);
+        } else {
+          console.log("RUNNING");
+          anime.anchor.set(0.5, 1);
+          anime.angle = 180;
+          if (gameData.currentStreak === 1) {
+            anime.x = app.screen.width / 2.38;
+          } else if (gameData.currentStreak === 2) {
+            anime.x = app.screen.width / 1.7175;
+          }
+          anime.y = 80;
+          anime.scale.set(1.75, 0.5);
+
+          anime.play();
+          app.stage.addChild(anime);
+          app.start();
+        }
+      });
+    }
+  }, [gameData.currentStreak]);
+
+  return (
+    <div
+      ref={ref}
+      className="absolute w-screen h-screen top-0 left-0 bg-transparent"
+    />
+  );
+};
+
+export default KillStreak;
