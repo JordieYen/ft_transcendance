68fd46465ecb6e1849284bdf719ed43dc2b7dc34
Shawn Teh <steh@u91z03s02.42kl.edu.my>
2023-08-29 13:32:25 +0800

    update

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["cdn.intra.42.fr", "localhost", process.env.NEXT_PUBLIC_HOST],
+    },
+  };
+  
+module.exports = nextConfig;
+  
